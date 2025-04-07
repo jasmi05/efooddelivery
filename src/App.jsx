d0044d8d2ca7a3940a2f71e5655722b4024cede6
jasmi05 <139744693+jasmi05@users.noreply.github.com>
2025-04-07 19:04:15 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,30 @@
-import React, { useState } from 'react'
-import Home from './pages/Home/Home'
-import Footer from './components/Footer/Footer'
+import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
 import { Route, Routes } from 'react-router-dom'
-import Cart from './pages/Cart/Cart'
-import LoginPopup from './components/LoginPopup/LoginPopup'
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
-import MyOrders from './pages/MyOrders/MyOrders'
-import { ToastContainer } from 'react-toastify';
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+import Add from './pages/Add/Add'
+import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Verify from './pages/Verify/Verify'
-
 const App = () => {
+ const url = "http://localhost:4000"
 
-  const [showLogin,setShowLogin] = useState(false);
 
   return (
-    <>
-    <ToastContainer/>
-    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
-      <div className='app'>
-        <Navbar setShowLogin={setShowLogin}/>
+    <div>
+      <ToastContainer/>
+      <Navbar/>
+      <hr/>
+      <div className="app-content">
+        <Sidebar/>
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/cart' element={<Cart />}/>
-          <Route path='/order' element={<PlaceOrder />}/>
-          <Route path='/myorders' element={<MyOrders />}/>
-          <Route path='/verify' element={<Verify />}/>
+          <Route path ="/add" element={<Add url={url}/>}/>
+          <Route path ="/list" element={<List url={url}/>}/>
+          <Route path ="/orders" element={<Orders url={url}/>}/>
         </Routes>
       </div>
-      <Footer />
-    </>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import StoreContextProvider from './context/StoreContext.jsx';
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
-
-// Replace with your actual Stripe public key
-const stripePromise = loadStripe('pk_test_51Q3gTfRrt2PQUqSbVEoOeGz9AVMcZnnUQuSoD0jBEEFeGjW1ySR41Cni3CoTkBCcXIimH0H0uERZ5s65s3m3V0la00T5f1Yd5r');
-
+//import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter} from "react-router-dom"
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <StoreContextProvider>
-      <Elements stripe={stripePromise}>
-        <App />
-      </Elements>
-    </StoreContextProvider>
-  </BrowserRouter>
-);
+    <App />
+    </BrowserRouter>
+)
